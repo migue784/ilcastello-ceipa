bf577304fa57be5c17dbf7f7e9b56a1c9653c314
¨migue784¨ <miguellopera@MacBook-Air-de-Miguel-4.local>
2026-04-27 21:05:48 -0500

    fix: correct package.json versions to stable published releases so Vercel can build

--- patch_package.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const pkgPath = './package.json';
-let pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
-pkg.devDependencies["@tailwindcss/vite"] = "^4.0.0";
-pkg.devDependencies["tailwindcss"] = "^4.0.0";
-fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
